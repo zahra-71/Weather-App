78a5377f3d42c5f07c30ec49c22e36dd03ebbc94
zahrasarihi <zahra.sarihi71@gmail.com>
2022-02-02 16:03:08 +0330

    Weather App with PWA

---
+++ src/api/FetchWeather.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "5d8a27f59afa89cde9db24885fc5075c";
+
+const FetchWeather = async (query) => {
+    const { data } = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+    
+    return data;
+}
+export default FetchWeather;
